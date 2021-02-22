5e2b901e0f07a3ffde2a7dff430a4b7dca8619a2
Marat <eshmakar@mail.ru>
2021-02-22 23:39:40 +0300

    Вычисляет весь человека на луне

---
+++ src/main/java/VesNaLune.java
@@ -0,0 +1,7 @@
+public class VesNaLune {
+    public static void main(String[] args) {
+        double zemlya = 64;
+        double luna = .17 * zemlya;
+        System.out.println("На земле вы " + zemlya + " кг, а на луне: " + luna);
+    }
+}
