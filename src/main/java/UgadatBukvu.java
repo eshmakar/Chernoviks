380b3097e340419ff945d37fdf8fe8b6b8a7f6ad
Marat <eshmakar@mail.ru>
2021-02-26 19:40:28 +0300

    Была добавлена игра "угадай букву", используется do while, if, else

---
+++ src/main/java/UgadatBukvu.java
@@ -0,0 +1,19 @@
+public class UgadatBukvu {
+    public static void main(String[] args) {
+        char ch, ignore, answer = 'K';
+        do {
+            System.out.println("Задумана буква из диапазона A-Z\nПопытайтесь его угадать: ");
+            ch = (char) System.in.read();
+            do ignore = (char) System.in.read();
+            while (ignore != '\n');
+
+            if (ch == answer) System.out.println("Правильно!");
+            else {
+                System.out.print("Нужная буква находится ");
+                if (ch < answer) System.out.println("ближе к концу алфавита");
+                else System.out.println("ближе к началу алфавита");
+                System.out.println("Повторите попытку!\n");
+            }
+        } while (answer != ch);
+    }
+}
